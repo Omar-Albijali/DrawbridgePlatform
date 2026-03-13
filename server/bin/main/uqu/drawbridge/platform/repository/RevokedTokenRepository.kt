432a1f85887357938ef4513056820c5a6b0263c9
Omar-Albijali <omaralpjaly@gmail.com>
2026-03-13 19:36:48 +0300

    Remove duplicate server

--- server/bin/main/uqu/drawbridge/platform/repository/RevokedTokenRepository.kt
+++
@@ -1,7 +0,0 @@
-package uqu.drawbridge.platform.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import uqu.drawbridge.platform.model.RevokedToken
-
-interface RevokedTokenRepository : JpaRepository<RevokedToken, String>
-
