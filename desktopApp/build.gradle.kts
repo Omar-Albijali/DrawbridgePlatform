4bc1ee6741c9fe93fef2eac10fc1a91d4bbf4755
OMAR ALBIJALI <omaralpjaly@gmail.com>
2026-05-26 06:31:53 +0300

    Separate the desktop application into `desktopApp` module (#59)
    * Add desktop target

---
+++ desktopApp/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+dependencies {
+    implementation(projects.composeApp)
+//    implementation(projects.shared)
+
+    implementation(compose.desktop.currentOs)
+    implementation(libs.kotlinx.coroutinesSwing)
+
+    implementation(libs.compose.uiToolingPreview)
+}
+
+compose.desktop {
+    application {
+        mainClass = "uqu.drawbridge.platform.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "uqu.drawbridge.platform"
+            packageVersion = "1.0.0"
+        }
+    }
+}
